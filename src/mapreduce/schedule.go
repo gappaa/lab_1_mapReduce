c549f96a8a62b79351d00ebbe7b109ffbfd35482
gappaa <marian1.gappa@epitech.eu>
2018-04-01 15:21:41 +0800

    chore(map/reduce): Pass part III and IV

--- src/mapreduce/schedule.go
+++ src/mapreduce/schedule.go
@@ -1,6 +1,16 @@
 package mapreduce
 
-import "fmt"
+import (
+	"fmt"
+	logger "github.com/sirupsen/logrus"
+	"sync"
+	"sync/atomic"
+)
+
+type Task struct {
+	file string
+	number int
+}
 
 //
 // schedule() starts and waits for all tasks in the given phase (mapPhase
@@ -22,8 +32,119 @@ func schedule(jobName string, mapFiles []string, nReduce int, phase jobPhase, re
 		ntasks = nReduce
 		n_other = len(mapFiles)
 	}
+	logger.WithFields(logger.Fields{
+		"nTasks": ntasks,
+		"nOther": n_other,
+	}).Info("[schedule] Init schedule")
 
+	workers := make(chan string, 10)
+	tasks := make(chan Task, ntasks)
+	stopWorker := make(chan struct{})
+	atomicIsClose := int32(0)
+
+	remainingTasks := &sync.WaitGroup{}
+	remainingTasks.Add(ntasks)
+	
 	fmt.Printf("Schedule: %v %v tasks (%d I/Os)\n", ntasks, phase, n_other)
+
+	// Add tasks
+	go func(tasksChan chan Task, mapFiles []string, nbTasks int) {
+		for cpt, file := range mapFiles {
+			if cpt < nbTasks {
+				logger.WithField("file", file).Info("[schedule] add task")
+				if atomic.LoadInt32(&atomicIsClose) == 0 {
+					tasks <- Task{file: file, number: cpt}
+				}
+			}
+		}
+	}(tasks, mapFiles, ntasks)
+
+	// Add workers
+	go func(workers chan string, register chan string, stopWorker chan struct{}) {
+		for {
+			select {
+			case element, ok := <-register:
+				if ok {
+					logger.WithField("worker", element).Info("[schedule] add worker")
+					if atomic.LoadInt32(&atomicIsClose) == 0 {
+						workers <- element
+					}
+				} else {
+					break
+				}
+			case <- stopWorker:
+				return
+			}
+		}
+	}(workers, registerChan, stopWorker)
+
+	// Give task
+	go func(workers chan string, tasks chan Task, waitGroup *sync.WaitGroup) {
+		for {
+			select {
+			case worker, ok := <- workers:
+				if !ok {
+					return
+				}
+				go func(worker string, tasks chan Task, workers chan string, waitGroup *sync.WaitGroup) {
+					select {
+					case taskFile, ok := <- tasks:
+						if !ok {
+							return
+						}
+						task := DoTaskArgs{
+							JobName: jobName,
+							Phase: phase,
+							TaskNumber: taskFile.number,
+							NumOtherPhase: n_other,
+						}
+
+						if phase == mapPhase {
+							task.File = taskFile.file
+						}
+
+						logger.WithFields(logger.Fields{
+							"worker": worker,
+							"task": task,
+						}).Info("[schedule] file send")
+
+						if call(worker, "Worker.DoTask", task, nil) {
+							logger.WithFields(logger.Fields{
+								"worker": worker,
+								"task": task,
+							}).Info("[schedule] task success")
+							if atomic.LoadInt32(&atomicIsClose) == 0 {
+								workers <- worker
+							}
+							waitGroup.Done()
+						} else {
+							logger.WithFields(logger.Fields{
+								"worker": worker,
+								"task": task,
+							}).Info("[schedule] task error")
+							if atomic.LoadInt32(&atomicIsClose) == 0 {
+								workers <- worker
+							}
+							if atomic.LoadInt32(&atomicIsClose) == 0 {
+								tasks <- taskFile
+							}
+						}
+					}
+
+				}(worker, tasks, workers, waitGroup)
+			}
+		}
+	}(workers, tasks, remainingTasks)
+
+	// Task all finished
+	remainingTasks.Wait()
+	logger.Info("[schedule] END")
+	atomic.AddInt32(&atomicIsClose, 1)
+	stopWorker <- struct{}{}
+	close(workers)
+	close(tasks)
+	close(stopWorker)
+
 
 	// All ntasks tasks have to be scheduled on workers. Once all tasks
 	// have completed successfully, schedule() should return.
